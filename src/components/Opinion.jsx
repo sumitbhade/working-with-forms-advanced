22255cf3b8625d8d930dea06c76e3c71b352046e
Sumit Bhade <imsumitbhade@gmail.com>
2025-01-21 00:35:34 +0530

    Sending HTTP Request via Form Action

--- src/components/Opinion.jsx
+++ src/components/Opinion.jsx
@@ -1,9 +1,14 @@
+import { use } from "react";
+import { OpinionsContext } from "../store/opinions-context";
+
 export function Opinion({ opinion: { id, title, body, userName, votes } }) {
-  function upvoteAction() {
-    console.log("UPVOTE");
+  const { upvoteOpinion, downvoteOpinion } = use(OpinionsContext);
+
+  async function upvoteAction() {
+    await upvoteOpinion(id);
   }
-  function downvoteAction() {
-    console.log("DOWNVOTE");
+  async function downvoteAction() {
+    await downvoteOpinion(id);
   }
 
   return (
